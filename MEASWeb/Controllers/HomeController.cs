0a0684294c6ce37af44a4e238597b6a4a97da6b1
work_user <work_user@sina.com>
2015-06-18 18:26:39 +0800

    添加树类

--- MEASWeb/Controllers/HomeController.cs
+++ MEASWeb/Controllers/HomeController.cs
@@ -175,25 +175,5 @@ namespace MEASWeb.Controllers
         public int type { get; set; }
     }
 
-    public class TreeNode
-    {
-        public TreeNode()
-        {
-            Checked = false;
-        }
-
-        public string Id { get; set; }
-
-        public string Text { get; set; }
-
-        public bool Checked { get; set; }
-
-        public int Order { get; set; }
-
-        public string IconCls { get; set; }
-
-        public string Url { get; set; }
-
-        public ICollection<TreeNode> Children { get; set; }
-    }
+   
 }
